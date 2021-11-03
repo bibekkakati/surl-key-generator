76420912f3b25fcf9b67739866cd5ddb23b07436
Bibek <bibekkakati0@gmail.com>
2021-11-04 01:17:26 +0530

    Minor fix

--- queries/key_range_list.js
+++ queries/key_range_list.js
@@ -8,62 +8,61 @@ const COL_RANGE = "range";
 const COL_SERVER_ID = "serverId";
 
 const createTable = async () => {
-	const data = JSON.stringify({
-		operation: "create_table",
-		schema: DB_NAME,
-		table: KEY_RANGE_TABLE,
-		hash_attribute: "id",
-	});
+    const data = JSON.stringify({
+        operation: "create_table",
+        schema: DB_NAME,
+        table: KEY_RANGE_TABLE,
+        hash_attribute: "id",
+    });
 
-	const config = { ...DB_CONFIG, data };
+    const config = { ...DB_CONFIG, data };
 
-	try {
-		const response = await request(config);
-		const { message, error } = response.data;
-		if (error) {
-			throw new Error(error);
-		} else {
-			console.log("CREATE TABLE INFO: " + message);
-		}
-	} catch (error) {
-		console.log("CATCH CREATE TABLE ERROR: " + error.message);
-		console.log("Check if table already exists");
-		process.exit(1);
-	}
+    try {
+        const response = await request(config);
+        const { message, error } = response.data;
+        if (error) {
+            throw new Error(error);
+        } else {
+            console.log("CREATE TABLE INFO: " + message);
+        }
+    } catch (error) {
+        console.log("CATCH CREATE TABLE ERROR: " + error.message);
+        console.log("Check if table already exists");
+    }
 };
 
 const insertServerInfo = async (from, to, serverId) => {
-	if (from < 0 || to < 0 || !serverId) {
-		throw new Error("INSERT SERVER INFO QUERY: args missing");
-	}
-	const range = from + ":" + to;
-	const data = JSON.stringify({
-		operation: "insert",
-		schema: DB_NAME,
-		table: KEY_RANGE_TABLE,
-		records: [
-			{
-				[COL_RANGE]: range,
-				[COL_SERVER_ID]: serverId,
-			},
-		],
-	});
+    if (from < 0 || to < 0 || !serverId) {
+        throw new Error("INSERT SERVER INFO QUERY: args missing");
+    }
+    const range = from + ":" + to;
+    const data = JSON.stringify({
+        operation: "insert",
+        schema: DB_NAME,
+        table: KEY_RANGE_TABLE,
+        records: [
+            {
+                [COL_RANGE]: range,
+                [COL_SERVER_ID]: serverId,
+            },
+        ],
+    });
 
-	const config = { ...DB_CONFIG, data };
+    const config = { ...DB_CONFIG, data };
 
-	try {
-		const response = await request(config);
-		const { inserted_hashes } = response.data;
-		if (inserted_hashes.length > 0) {
-			return [id, null];
-		}
-		return [null, null];
-	} catch (error) {
-		return [null, "Something went wrong"];
-	}
+    try {
+        const response = await request(config);
+        const { inserted_hashes } = response.data;
+        if (inserted_hashes.length > 0) {
+            return [id, null];
+        }
+        return [null, null];
+    } catch (error) {
+        return [null, "Something went wrong"];
+    }
 };
 
 module.exports = {
-	createTable,
-	insertServerInfo,
+    createTable,
+    insertServerInfo,
 };
